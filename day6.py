90adcd8e46fee8e1abd35b807a90a69396cb04a2
Om Retharekar <omretharekar@gmail.com>
2026-04-07 10:47:09 +0530

    Day6 material

---
+++ day6.py
@@ -0,0 +1,33 @@
+#WAP to compress a string by replacing consecutive characters with their counts
+#input = "aaabbbcccc"
+#output = "a3b3c4"
+# s = "aaabbbcccc"
+# a = ""
+# for i in s:
+#     if i not in a:
+#         a += i
+#         a += str(s.count(i))
+# print(s)
+# print(a)
+
+#----------------------------------------------------------------------------------------------------------------------------------------------
+
+#WAP to reverse each word in a string
+
+# s = "Hello World"
+# a = s.split()
+# print (a)
+# b = ""
+# for i in a:
+#     b += i[::-1] + " "
+# print(s)
+# print(b)
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------
+
+# Abstraction
+
+'''
+A class which contains one or more abstract methods is called an abstract class.
+An abstract method is a method that has a declaration but does not have an implementation.
+'''
